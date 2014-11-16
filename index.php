266dfb5ec5ebafeda86f992df04552c3c788f30e
shimarahman <rahmanshima44@gmail.com>
2014-11-16 10:17:22 +0600

    write the hello world

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-$array1 = array("color" => "green", 2, 4);
-$array2 = array("a", "b", "color" => "red", "shape" => "trapezoid", 4);
-$result = array_merge($array1, $array2);
-print_r($result);
+echo "hello world";
+echo "hello world";
+echo "hello world";
+echo "hello world";
+echo "hello world";
 ?>
